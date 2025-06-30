22c4cb292b61a93dd5e1ca030bcbc4f873552dbd
Maen KH <m.alkhraisha@sakan.co>
2025-07-01 01:26:12 +0300

    fix

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,6 +4,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import ThemeView from "../components/ThemeView";
 import Sidebar from "./sidebar";
+import { View } from "react-native";
 
 export default function RootLayout() {
     return (
@@ -12,18 +13,20 @@ export default function RootLayout() {
                 <StatusBar style='auto' />
                 <ThemeView style={{ flex: 1 }}>
                     <Sidebar />
-                    <Stack
-                        screenOptions={{
-                            headerShown: false,
-                        }}>
-                        {/* Public routes */}
-                        <Stack.Screen name='index' />
-                        <Stack.Screen name='(auth)' />
-                        <Stack.Screen name='about' />
+                    <View style={{ flex: 1 }}>
+                        <Stack
+                            screenOptions={{
+                                headerShown: false,
+                            }}>
+                            {/* Public routes */}
+                            <Stack.Screen name='index' />
+                            <Stack.Screen name='(auth)' />
+                            <Stack.Screen name='about' />
 
-                        {/* Protected routes group */}
-                        <Stack.Screen name='(UserOnly)' />
-                    </Stack>
+                            {/* Protected routes group */}
+                            <Stack.Screen name='(UserOnly)' />
+                        </Stack>
+                    </View>
                 </ThemeView>
             </SafeAreaProvider>
         </UserProvider>
